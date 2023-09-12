ec5669c6c84787353452f8a2883d23d37419f886
viitran <tieuvi200904@gmail.com>
2023-09-12 13:23:41 +0700

    Nop bai tap Bien&Kieudl&Toantu

---
+++ module 1/ss7_toantu/baitap/tinhdiemtb.js
@@ -0,0 +1,8 @@
+let a;
+let b;
+let c;
+a= +prompt(Nhập điểm môn Toán')
+b= +prompt(Nhập điểm môn Lý')
+c= +prompt(Nhập điểm môn Hóa')
+Sum = ( a + b + c ) / 3;
+alert(Sum);
---
+++ module 1/ss7_toantu/thuchanh/th_sudungtoantu.js
@@ -0,0 +1,6 @@
+inputWidth = prompt('Nhập Chiều rộng');
+inputHeight = prompt('Nhập Chiều cao');
+let width =+inputWidth;
+let height = +inputHeight;
+let S= width * height
+document.write('S = '+S)
