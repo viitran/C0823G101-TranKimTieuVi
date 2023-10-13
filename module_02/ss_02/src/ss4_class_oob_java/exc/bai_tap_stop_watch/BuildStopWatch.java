217113173b248f3f0ab77d8a3b856f83b99077bd
viitran <tieuvi200904@gmail.com>
2023-10-13 12:34:05 +0700

    Nop bai  fix bai tap AM

--- module_02/ss_02/src/ss4_class_oob_java/exc/bai_tap_stop_watch/BuildStopWatch.java
+++ module_02/ss_02/src/ss4_class_oob_java/exc/bai_tap_stop_watch/BuildStopWatch.java
@@ -2,6 +2,13 @@ package ss4_class_oob_java.exc.bai_tap_stop_watch;
 
 public class BuildStopWatch {
     public static void main(String[] args) {
-
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        System.out.println("Đang đếm thời gian");
+        for (int i = 0; i < 1000000000; i++) {
+            i++;
+        }
+        stopWatch.stop();
+        stopWatch.getElapsedTime();
     }
 }
--- module_02/ss_02/src/ss4_class_oob_java/exc/bai_tap_stop_watch/StopWatch.java
+++ module_02/ss_02/src/ss4_class_oob_java/exc/bai_tap_stop_watch/StopWatch.java
@@ -1,16 +1,16 @@
 package ss4_class_oob_java.exc.bai_tap_stop_watch;
 
 public class StopWatch {
-    //fix lại - bị dư 1 vài chỗ + NOTE: chú ý get/set
+    //fix lại - bị dư 1 vài chỗ + NOTE: chú ý get
     private long startTime;
     private long endTime;
 
-    public void setStartTime(long startTime) {
-        this.startTime = startTime;
+    public long getStartTime() {
+        return this.startTime;
     }
 
-    public void setEndTime(long endTime) {
-        this.endTime = endTime;
+    public long getEndTime() {
+        return this.endTime;
     }
 
     StopWatch() {
@@ -29,16 +29,5 @@ public class StopWatch {
         long elaspedTime = this.endTime - this.startTime;
         System.out.println("Số mili giây đếm đc:" + elaspedTime);
     }
-
-
-    public static void main(String[] args) {
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        System.out.println("Đang đếm thời gian");
-        for (int i = 0; i < 1000000000; i++) {
-            i++;
-        }
-        stopWatch.stop();
-        stopWatch.getElapsedTime();
-    }
 }
+
