ad735ddaf0ec51e37262ae1a50e1a693b57df023
viitran <tieuvi200904@gmail.com>
2023-10-27 20:08:53 +0700

    nop bai tap ss15 exception

---
+++ module_02/ss_02/src/ss15_exception_java/exc/CheckTriangleException.java
@@ -0,0 +1,51 @@
+package ss15_exception_java.exc;
+
+import java.util.Scanner;
+
+public class CheckTriangleException {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static void displayException(double a, double b, double c) throws Exception {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalTriangleException("Cạnh của tam giác không được bé hơn 1");
+        } else if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalTriangleException("Cạnh của tam giác không hợp lệ!");
+        }
+        System.out.println("Tam giác hợp lệ");
+    }
+
+    private static Double checkNumber(String side) {
+        while (true) {
+            try {
+                double value = Double.parseDouble(scanner.nextLine());
+                if (value <= 0) {
+                    System.out.print("Cạnh của tam giác phải là 1 số dương. Vui lòng nhập lại cạnh " + side + " : ");
+                } else {
+                    return value;
+                }
+            } catch (NumberFormatException e) {
+                System.out.print("Lỗi: Vui lòng nhập một số hợp lệ cho cạnh " + side + ": ");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        double a;
+        double b;
+        double c;
+        do {
+            try {
+                System.out.print("Nhập cạnh a: ");
+                a = checkNumber("a");
+                System.out.print("Nhập cạnh b: ");
+                b = checkNumber("b");
+                System.out.print("Nhập cạnh c: ");
+                c = checkNumber("c");
+                displayException(a, b, c);
+            } catch (Exception e) {
+                System.out.println("Lỗi: " + e.getMessage());
+            }
+        } while (true);
+    }
+}
+
---
+++ module_02/ss_02/src/ss15_exception_java/exc/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+package ss15_exception_java.exc;
+
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String string) {
+        super(string);
+    }
+}
