d82d439fb21be8e1858c59c6982951d319450e7d
viitran <tieuvi200904@gmail.com>
2023-11-16 19:16:39 +0700

    upd casestudy

--- Case_study/module_03/furama_db.sql
+++ Case_study/module_03/furama_db.sql
@@ -1,73 +1,218 @@
-create database case_study_furama;
-use case_study_furama;
-
-CREATE TABLE trinh_do (
-    ma INT PRIMARY KEY,
-    ten_bang_cap VARCHAR(30)
-);
+CREATE DATABASE case_study_furama;
+USE case_study_furama;
 
 CREATE TABLE vi_tri (
-    ma INT PRIMARY KEY,
-    ten_vi_tri VARCHAR(30)
+    ma_vi_tri INT PRIMARY KEY AUTO_INCREMENT,
+    ten_vi_tri VARCHAR(45)
 );
 
-CREATE TABLE bo_phan_lam_viec (
-    ma INT PRIMARY KEY,
-    ten_bo_phan VARCHAR(30)
+CREATE TABLE trinh_do (
+    ma_trinh_do INT PRIMARY KEY AUTO_INCREMENT,
+    ten_trinh_do VARCHAR(45)
 );
 
-create table loai_khach(
-	ma int primary key,
-    ten_loai_khach varchar(30)
+CREATE TABLE bo_phan (
+    ma_bo_phan INT PRIMARY KEY AUTO_INCREMENT,
+    ten_bo_phan VARCHAR(45)
 );
 
 CREATE TABLE nhan_vien (
-    ma VARCHAR(20) primary key,
-    `name` VARCHAR(50),
-    birthday DATE,
-    identify VARCHAR(20),
-    phone_number INT(20),
-    email VARCHAR(30),
-    trinh_do_ma INT,
-    vi_tri_ma INT,
-    bo_phan_lam_viec_ma INT,
-    FOREIGN KEY (trinh_do_ma)
-        REFERENCES trinh_do (ma),
-    FOREIGN KEY (vi_tri_ma)
-        REFERENCES vi_tri (ma),
-    FOREIGN KEY (bo_phan_lam_viec_ma)
-        REFERENCES bo_phan_lam_viec (ma)
+    ma_nhan_vien INT PRIMARY KEY AUTO_INCREMENT,
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    luong DOUBLE NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    email VARCHAR(45),
+    dia_chi VARCHAR(45),
+    ma_vi_tri INT NOT NULL,
+    ma_trinh_do INT NOT NULL,
+    ma_bo_phan INT NOT NULL,
+    FOREIGN KEY (ma_vi_tri)
+        REFERENCES vi_tri (ma_vi_tri),
+    FOREIGN KEY (ma_trinh_do)
+        REFERENCES trinh_do (ma_trinh_do),
+    FOREIGN KEY (ma_bo_phan)
+        REFERENCES bo_phan (ma_bo_phan)
 );
 
-create table khach_hang(
-	ma varchar(30) primary key,
-    `name` varchar(50),
-    birthday date,
-    gender varchar(5),
-    identify varchar(20),
-    phone_number int(20),
-    email varchar(30),
-    dia_chi varchar(30),
-    loai_khach_ma int,
-    foreign key(loai_khach_ma) references loai_khach (ma)
+CREATE TABLE loai_khach (
+    ma_loai_khach INT PRIMARY KEY AUTO_INCREMENT,
+    ten_loai_khach VARCHAR(45)
 );
 
-create table dich_vu(
-	ma int primary key,
-    ten_dich_vu varchar(30)
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY AUTO_INCREMENT,
+    ma_loai_khach INT NOT NULL,
+    FOREIGN KEY (ma_loai_khach)
+        REFERENCES loai_khach (ma_loai_khach),
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    gioi_tinh BIT(1) NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    email VARCHAR(45),
+    dia_chi VARCHAR(45)
 );
 
-create table kieu_thue(
-	ma int primary key,
-    ten_kieu_thue varchar(30)
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT PRIMARY KEY AUTO_INCREMENT,
+    ten_kieu_thue VARCHAR(45)
 );
 
-create table khu_nghi_duong(
- ma int primary key,
- dich_vu_ma int,
- dien_tich_su_dung double,
- so_luong_nguoi_toi_da int,
- kieu_thue_ma int,
- foreign key (kieu_thue_ma) references kieu_thue(ma),
- foreign key(dich_vu_ma) references dich_vu(ma)
+CREATE TABLE loai_dich_vu (
+    ma_loai_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+    ten_loai_dich_vu VARCHAR(45)
 );
+
+CREATE TABLE dich_vu (
+    ma_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+    ten_dich_vu VARCHAR(45) NOT NULL,
+    dien_tich INT,
+    chi_phi_thue DOUBLE NOT NULL,
+    so_nguoi_toi_da INT,
+    ma_kieu_thue INT NOT NULL,
+    FOREIGN KEY (ma_kieu_thue)
+        REFERENCES kieu_thue (ma_kieu_thue),
+    ma_loai_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_loai_dich_vu)
+        REFERENCES loai_dich_vu (ma_loai_dich_vu),
+    tieu_chuan_phong VARCHAR(45),
+    mo_ta_tien_nghi_khac VARCHAR(45),
+    dien_tich_ho_boi DOUBLE,
+    so_tang INT,
+    dich_vu_mien_phu_di_kem TEXT
+);
+
+CREATE TABLE dich_vu_di_kem (
+    ma_dich_vu_di_kem INT PRIMARY KEY AUTO_INCREMENT,
+    ten_dich_vu_di_kem VARCHAR(45) NOT NULL,
+    gia DOUBLE NOT NULL,
+    don_vi VARCHAR(10) NOT NULL,
+    trang_thai VARCHAR(45)
+);
+
+CREATE TABLE hop_dong (
+    ma_hop_dong INT PRIMARY KEY AUTO_INCREMENT,
+    ngay_lam_hop_dong DATETIME NOT NULL,
+    ngay_ket_thuc DATETIME NOT NULL,
+    tien_dat_coc DOUBLE NOT NULL,
+    ma_nhan_vien INT NOT NULL,
+    FOREIGN KEY (ma_nhan_vien)
+        REFERENCES nhan_vien (ma_nhan_vien),
+    ma_khach_hang INT NOT NULL,
+    FOREIGN KEY (ma_khach_hang)
+        REFERENCES khach_hang (ma_khach_hang),
+    ma_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_dich_vu)
+        REFERENCES dich_vu (ma_dich_vu)
+);
+
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT PRIMARY KEY AUTO_INCREMENT,
+    ma_hop_dong INT NOT NULL,
+    FOREIGN KEY (ma_hop_dong)
+        REFERENCES hop_dong (ma_hop_dong),
+    ma_dich_vu_di_kem INT NOT NULL,
+    FOREIGN KEY (ma_dich_vu_di_kem)
+        REFERENCES dich_vu_di_kem (ma_dich_vu_di_kem),
+    so_luong INT NOT NULL
+);
+
+INSERT INTO vi_tri(ten_vi_tri)
+VALUES ('Quản lý'),
+('Nhân viên');
+
+INSERT INTO trinh_do(ten_trinh_do)
+VALUES ('Trung cấp'),
+('Cao đẳng'),
+('Đại học'),
+('Sau đại học');
+
+INSERT INTO bo_phan(ten_bo_phan) 
+VALUES ('Sale-Marketing'),
+('Hành chính'),
+('Phục vụ'),
+('Quản lý');
+
+INSERT INTO loai_khach(ten_loai_khach) 
+VALUES ('Diamond'),
+('Platinium'),
+('Gold'),
+('Silver'),
+('Member');
+
+INSERT INTO kieu_thue(ten_kieu_thue) X
+VALUES ('year'),
+('month'),
+('day'),
+('hour');
+
+INSERT INTO loai_dich_vu(ten_loai_dich_vu) 
+VALUES ('Villa'),
+('House'),
+('Room');
+
+INSERT INTO dich_vu_di_kem(ten_dich_vu_di_kem,gia,don_vi,trang_thai)
+VALUES ('Karaoke',10000,'giờ','tiện nghi, hiện tại'),
+('Thuê xe máy',10000,'chiếc','hỏng 1 xe'),
+('Thuê xe đạp',20000,'chiếc','tốt'),
+('Buffet buổi sáng',15000,'suất','đầy đủ đồ ăn, tráng miệng'),
+('Buffet buổi trưa',90000,'suất','đầy đủ đồ ăn, tráng miệng'),
+('Buffet buổi tối',16000,'suất','đầy đủ đồ ăn, tráng miệng');
+
+INSERT INTO dich_vu(ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,ma_kieu_thue,ma_loai_dich_vu)
+VALUES ('Villa Beach Front',25000,10000000,10,'vip','Có hồ bơi',500,4,3,1),
+('House Princess 01',14000,5000000,7,'vip','Có thêm bếp nướng',NULL,3,2,2),
+('Room Twin 01',5000,1000000,2,'normal','Có tivi',NULL,NULL,4,3),
+('Villa No Beach Front',22000,9000000,8,'normal','Có hồ bơi',300,3,3,1),
+('House Princess 02',10000,4000000,5,'normal','Có thêm bếp nướng',NULL,2,3,2),
+('Room Twin 02',3000,900000,2,'normal','Có tivi',NULL,NULL,4,3);
+
+INSERT INTO nhan_vien(ho_ten,ngay_sinh,so_cmnd,luong,so_dien_thoai,email,dia_chi,ma_vi_tri,ma_trinh_do,ma_bo_phan) 
+VALUES ('Nguyễn Văn An','1970-11-07','456231786',10000000,'0901234121','annguyen@gmail.com','295 Nguyễn Tất Thành, Đà Nẵng',1,3,1),
+('Lê Văn Bình','1997-04-09','654231234',7000000,'0934212314','binhlv@gmail.com','22 Yên Bái, Đà Nẵng',1,2,2),
+('Hồ Thị Yến','1995-12-12','999231723',14000000,'0412352315','thiyen@gmail.com','K234/11 Điện Biên Phủ, Gia Lai',1,3,2),
+('Võ Công Toản','1980-04-04','123231365',17000000,'0374443232','toan0404@gmail.com','77 Hoàng Diệu, Quảng Trị',1,4,4),
+('Nguyễn Bỉnh Phát','1999-12-09','454363232',6000000,'0902341231','phatphat@gmail.com','43 Yên Bái, Đà Nẵng',2,1,1),
+('Khúc Nguyễn An Nghi','2000-11-08','964542311',7000000,'0978653213','annghi20@gmail.com','294 Nguyễn Tất Thành, Đà Nẵng',2,2,3),
+('Nguyễn Hữu Hà','1993-01-01','534323231',8000000,'0941234553','nhh0101@gmail.com','4 Nguyễn Chí Thanh, Huế',2,3,2),
+('Nguyễn Hà Đông','1989-09-03','234414123',9000000,'0642123111','donghanguyen@gmail.com','111 Hùng Vương, Hà Nội',2,4,4),
+('Tòng Hoang','1982-09-03','256781231',6000000,'0245144444','hoangtong@gmail.com','213 Hàm Nghi, Đà Nẵng',2,4,4),
+('Nguyễn Công Đạo','1994-01-08','755434343',8000000,'0988767111','nguyencongdao12@gmail.com','6 Hoà Khánh, Đồng Nai',2,3,2);
+
+INSERT INTO khach_hang(ho_ten,ngay_sinh,gioi_tinh,so_cmnd,so_dien_thoai,email,dia_chi,ma_loai_khach) 
+VALUES ('Nguyễn Thị Hào','1970-11-07',0,'643431213','0945423362','thihao07@gmail.com','23 Nguyễn Hoàng, Đà Nẵng',5),
+('Phạm Xuân Diệu','1992-08-08',1,'865342123','0954333333','xuandieu92@gmail.com','K77/22 Thái Phiên, Quảng Trị',3),
+('Trương Đình Nghệ','1990-02-27',1,'488645199','0373213122','nghenhan2702@gmail.com','K323/12 Ông Ích Khiêm, Vinh',1),
+('Dương Văn Quan','1981-07-08',1,'543432111','0490039241','duongquan@gmail.com','K453/12 Lê Lợi, Đà Nẵng',1),
+('Hoàng Trần Nhi Nhi','1995-12-09',0,'795453345','0312345678','nhinhi123@gmail.com','224 Lý Thái Tổ, Gia Lai',4),
+('Tôn Nữ Mộc Châu','2005-12-06',0,'732434215','0988888844','tonnuchau@gmail.com','37 Yên Thế, Đà Nẵng',4),
+('Nguyễn Mỹ Kim','1984-04-08',0,'856453123','0912345698','kimcuong84@gmail.com','K123/45 Lê Lợi, Hồ Chí Minh',1),
+('Nguyễn Thị Hào','1999-04-08',0,'965656433','0763212345','haohao99@gmail.com','55 Nguyễn Văn Linh, Kon Tum',3),
+('Trần Đại Danh','1994-07-01',1,'432341235','0643343433','danhhai99@gmail.com','24 Lý Thường Kiệt, Quảng Ngãi',1),
+('Nguyễn Tâm Đắc','1989-07-01',1,'344343432','0987654321','dactam@gmail.com','22 Ngô Quyền, Đà Nẵng',2);
+
+INSERT INTO hop_dong(ngay_lam_hop_dong,ngay_ket_thuc,tien_dat_coc,ma_nhan_vien,ma_khach_hang,ma_dich_vu) 
+VALUES ('2020-12-08','2020-12-08',0,3,1,3),
+('2020-07-14','2020-07-21',200000,7,3,1),
+('2021-03-15','2021-03-17',50000,3,4,2),
+('2021-01-14','2021-01-18',100000,7,5,5),
+('2021-07-14','2021-07-15',0,7,2,6),
+('2021-06-01','2021-06-03',0,7,7,6),
+('2021-09-02','2021-09-05',100000,7,4,4),
+('2021-06-17','2021-06-18',150000,3,4,1),
+('2020-11-19','2020-11-19',0,3,4,3),
+('2021-04-12','2021-04-14',0,10,3,5),
+('2021-04-25','2021-04-25',0,2,2,1),
+('2021-05-25','2021-05-27',0,7,10,1);
+
+INSERT INTO hop_dong_chi_tiet(so_luong,ma_hop_dong,ma_dich_vu_di_kem) 
+VALUES (5,2,4),
+(8,2,5),
+(15,2,6),
+(1,3,1),
+(11,3,2),
+(1,1,3),
+(2,1,2),
+(2,12,2);
