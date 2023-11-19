ba40a20ea76d391dafe2633ce1c4a64407e283e0
viitran <tieuvi200904@gmail.com>
2023-11-19 20:59:23 +0700

    upd case

--- Case_study/module_03/query_furama.sql
+++ Case_study/module_03/query_furama.sql
@@ -38,15 +38,96 @@ WHERE
 GROUP BY hop_dong.ma_khach_hang
 ORDER BY so_lan_dat_phong;
 
--- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
+-- câu 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
 -- (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
 -- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra). 
-select khach_hang.ma_khach_hang,khach_hang.ho_ten,loai_khach.ten_loai_khach,sum(dich_vu.chi_phi_thue +(dich_vu_di_kem.gia * hop_dong_chi_tiet.so_luong)) as 'tổng tiền' 
-from loai_khach
-left join khach_hang on khach_hang.ma_khach_hang = loai_khach.ma_loai_khach
-left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_hop_dong
+SELECT 
+    khach_hang.ma_khach_hang,
+    khach_hang.ho_ten,
+    loai_khach.ten_loai_khach,
+    SUM(dich_vu.chi_phi_thue + (dich_vu_di_kem.gia * hop_dong_chi_tiet.so_luong)) AS 'tổng tiền'
+FROM
+    loai_khach
+        LEFT JOIN
+    khach_hang ON khach_hang.ma_khach_hang = loai_khach.ma_loai_khach
+        LEFT JOIN
+    hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_hop_dong
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+        LEFT JOIN
+    dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_loai_dich_vu
+GROUP BY khach_hang.ma_khach_hang , loai_khach.ten_loai_khach , hop_dong.ma_hop_dong , dich_vu.ten_dich_vu , hop_dong.ngay_lam_hop_dong , hop_dong.ngay_ket_thuc;
+
+-- câu 6: 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue
+--  ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+
+SELECT 
+    dich_vu.ma_dich_vu,
+    dich_vu.ten_dich_vu,
+    dich_vu.dien_tich,
+    dich_vu.chi_phi_thue,
+    loai_dich_vu.ten_loai_dich_vu
+FROM
+    dich_vu
+        JOIN
+    hop_dong ON dich_vu.ma_dich_vu = hop_dong.ma_hop_dong
+        JOIN
+    loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+WHERE
+    hop_dong.ngay_lam_hop_dong > '2021-01-01';
+    
+    -- câu 7: 	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich
+    -- so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã 
+    -- từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+    
+    SELECT 
+    dich_vu.ma_dich_vu,
+    dich_vu.ten_dich_vu,
+    dich_vu.dien_tich,
+    dich_vu.so_nguoi_toi_da,
+    dich_vu.chi_phi_thue,
+    dich_vu.ten_dich_vu
+FROM
+    dich_vu
+        JOIN
+    hop_dong ON dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+        JOIN
+    loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+WHERE
+    YEAR(hop_dong.ngay_lam_hop_dong) < 2021
+    group by dich_vu.ma_dich_vu;
+    
+    -- --8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+
+-- cách 1 
+SELECT 
+    khach_hang.ho_ten
+FROM
+    khach_hang
+GROUP BY ho_ten;
+
+--  9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+SELECT 
+    MONTH(hop_dong.ngay_lam_hop_dong) AS 'tháng',
+    COUNT(khach_hang.ho_ten) AS 'số lượng khách hàng'
+FROM
+    hop_dong
+        JOIN
+    khach_hang ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+WHERE
+    YEAR(hop_dong.ngay_lam_hop_dong) = 2021
+GROUP BY MONTH(hop_dong.ngay_lam_hop_dong)
+ORDER BY MONTH(hop_dong.ngay_lam_hop_dong);
+
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm.
+--  Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+select hop_dong.ma_hop_dong,hop_dong.ngay_lam_hop_dong,hop_dong.ngay_ket_thuc,hop_dong.tien_dat_coc,sum(hop_dong_chi_tiet.so_luong)
+from hop_dong
 left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
-left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
-left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_loai_dich_vu
-order by khach_hang.ma_khach_hang
+group by hop_dong.ma_hop_dong
+
 
